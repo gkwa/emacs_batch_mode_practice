3fac1295d5b759f8d7bff7d2ca009aa448cb46a7
taylor <tailor@u.washington.edu>
2014-05-11 14:38:59 -0700

    Add Elpa and Melpa package repositories

--- elpa_marmelade_auto_install/emacs-install-clean.el
+++ elpa_marmelade_auto_install/emacs-install-clean.el
@@ -19,7 +19,9 @@
            ".elpa"))
     (setq package-archives
           '(("gnu" . "http://elpa.gnu.org/packages/")
-            ("marmalade" . "http://marmalade-repo.org/packages/")))
+            ("marmalade" . "http://marmalade-repo.org/packages/")
+	    ("melpa" . "http://melpa.milkbox.net/packages/")
+            ("elpa" . "http://tromey.com/elpa/")))
     (package-initialize)
     (package-refresh-contents)
     (if (and (file-exists-p (expand-file-name package))
