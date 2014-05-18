767305171c857c42f1bcc599cedba3997b1b42b5
taylor <tailor@u.washington.edu>
2014-05-18 14:33:36 -0700

    Wrap long elisp lines

--- elpa_marmelade_auto_install/emacs-install-clean.el
+++ elpa_marmelade_auto_install/emacs-install-clean.el
@@ -6,28 +6,29 @@
 
 (defun main ()
   (interactive)
-  (destructuring-bind (package &optional elpa-parent) command-line-args-left
-    ;; Make the elpa dir for this if we need to.
-    (when (and elpa-parent
-               (not (file-exists-p elpa-parent)))
-      (make-directory elpa-parent t))
-    ;; Package stuff
-    (setq package-user-dir
-          (concat
-           (or (concat elpa-parent "/")
-               user-emacs-directory)
-           ".elpa"))
-    (setq package-archives
-          '(("gnu" . "http://elpa.gnu.org/packages/")
-	    ("melpa" . "http://melpa.milkbox.net/packages/")
-            ("marmalade" . "http://marmalade-repo.org/packages/")
-            ("elpa" . "http://tromey.com/elpa/")))
-    (package-initialize)
-    (package-refresh-contents)
-    (if (and (file-exists-p (expand-file-name package))
-             (not (file-directory-p (expand-file-name package))))
-        (package-install-file (expand-file-name package))
-      ;; Else must just be a package
-      (package-install (intern package)))))
+  (destructuring-bind
+   (package &optional elpa-parent) command-line-args-left
+   ;; Make the elpa dir for this if we need to.
+   (when (and elpa-parent
+	      (not (file-exists-p elpa-parent)))
+     (make-directory elpa-parent t))
+   ;; Package stuff
+   (setq package-user-dir
+	 (concat
+	  (or (concat elpa-parent "/")
+	      user-emacs-directory)
+	  ".elpa"))
+   (setq package-archives
+	 '(("gnu" . "http://elpa.gnu.org/packages/")
+	   ("melpa" . "http://melpa.milkbox.net/packages/")
+	   ("marmalade" . "http://marmalade-repo.org/packages/")
+	   ("elpa" . "http://tromey.com/elpa/")))
+   (package-initialize)
+   (package-refresh-contents)
+   (if (and (file-exists-p (expand-file-name package))
+	    (not (file-directory-p (expand-file-name package))))
+       (package-install-file (expand-file-name package))
+     ;; Else must just be a package
+     (package-install (intern package)))))
 
 ;; End
