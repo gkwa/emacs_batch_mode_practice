911adbb22493dde369ef2b36e4149ab3263ec2b7
demo <demo@demos-MacBook-Pro-2.local>
2014-05-18 11:30:33 -0700

    Right now, marmelate-repo.org site is down, comment out this package source

--- minimal-package-test/init.el
+++ minimal-package-test/init.el
@@ -4,7 +4,7 @@
 ;; Add the original Emacs Lisp Package Archive
 (add-to-list 'package-archives '("elpa" . "http://tromey.com/elpa/"))
 ;; Add the user-contributed repository
-(add-to-list 'package-archives '("marmalade" . "http://marmalade-repo.org/packages/"))
+;; (add-to-list 'package-archives '("marmalade" . "http://marmalade-repo.org/packages/"))
 (add-to-list 'package-archives '("melpa" . "http://melpa.milkbox.net/packages/"))
 (package-initialize)
 
