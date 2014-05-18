94ac4773aa145aacb6186f2380391572b1929977
taylor <tailor@u.washington.edu>
2014-05-18 14:41:43 -0700

    Fix spelling

--- elpa_marmelade_auto_install/emacs-install-clean.el
+++ elpa_marmelade_auto_install/emacs-install-clean.el
@@ -21,7 +21,7 @@
 
    (setq package-archives ())
    (add-to-list 'package-archives '("elpa" . "http://tromey.com/elpa/"))
-;; Marmelade site is down now
+;; Marmalade site is down now
 ;;   (add-to-list 'package-archives '("marmalade" . "http://marmalade-repo.org/packages/"))
    (add-to-list 'package-archives '("melpa" . "http://melpa.milkbox.net/packages/"))
 
