f21aff87c2967ef188e57e397357008db10fcc1f
taylormonacelli <tailor@u.washington.edu>
2014-08-24 15:25:02 -0700

    yada-yada

--- automated-testing-packages/build.el
+++ automated-testing-packages/build.el
@@ -7,9 +7,11 @@
 (add-to-list 'package-archives '("marmalade" . "http://marmalade-repo.org/packages/"))
 (add-to-list 'package-archives '("melpa" . "http://melpa.milkbox.net/packages/"))
 (add-to-list 'package-archives '("melpa-stable" . "http://melpa-stable.milkbox.net/packages/") t) ;; http://emacsredux.com/blog/2014/05/16/melpa-stable
-
 (package-initialize)
-(package-refresh-contents)
+
+(when (not package-archive-contents)
+  (package-refresh-contents))
+
 (let ((package
        (concat
         (file-name-directory
