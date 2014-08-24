c8bc68979e537b4834216cdd6be60c3618836ffd
taylormonacelli <tailor@u.washington.edu>
2014-08-24 15:53:45 -0700

    yada-yada

--- automated-testing-packages/build4.el
+++ automated-testing-packages/build4.el
@@ -1,3 +1,4 @@
+					; http://stackoverflow.com/questions/10092322/how-to-automatically-install-emacs-packages-by-specifying-a-list-of-package-name
 ;; https://github.com/slipset/emacs/blob/master/ensure-packages.el
 
 (require 'package)
@@ -6,32 +7,32 @@
 
 (setq url-http-attempt-keepalives nil)
 
-
-
 (defvar ensure-packages
-      '(
-        ac-dabbrev
-        yasnippet
-        )
+  '(
+    ac-dabbrev
+    yasnippet
+    )
   "A list of packages to ensure are installed at launch.")
 
 (defun ensure-packages-package-installed-p (p)
-  (cond ((package-installed-p p) t)
-	(t nil)))
+  (progn
+    (message "checking")
+    (cond ((package-installed-p p) t)
+	  (t nil)))
 
-(defun ensure-packages-installed-p ()
-  (mapcar 'ensure-packages-package-installed-p ensure-packages))
+  (defun ensure-packages-installed-p ()
+    (mapcar 'ensure-packages-package-installed-p ensure-packages))
 
-(defun ensure-packages-install-missing ()
-  (interactive)
-  (unless (every 'identity (ensure-packages-installed-p))
-    ;; check for new packages (package versions)
-    (message "%s" "Emacs is now refreshing its package database...")
-    (package-refresh-contents)
-    (message "%s" " done.")
-    ;; install the missing packages
-    (dolist (p ensure-packages)
-      (when (not (package-installed-p p))
-	(package-install p)))))
+  (defun ensure-packages-install-missing ()
+    (interactive)
+    (unless (every 'identity (ensure-packages-installed-p))
+      ;; check for new packages (package versions)
+      (message "%s" "Emacs is now refreshing its package database...")
+      (package-refresh-contents)
+      (message "%s" " done.")
+      ;; install the missing packages
+      (dolist (p ensure-packages)
+	(when (not (package-installed-p p))
+	  (package-install p)))))
 
-(provide 'ensure-packages)
+  (provide 'ensure-packages)
