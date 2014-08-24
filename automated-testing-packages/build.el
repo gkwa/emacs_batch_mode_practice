f488f6d8d74e4a9b2ebc917ddc1701f30281ba2e
taylormonacelli <tailor@u.washington.edu>
2014-08-24 13:31:50 -0700

    yada-yada

--- automated-testing-packages/build.el
+++ automated-testing-packages/build.el
@@ -1,3 +1,7 @@
+;; Load Package
+(require 'cl)
+(require 'package)
+
 (setq package-archives ())
 (add-to-list 'package-archives '("elpa" . "http://tromey.com/elpa/"))
 (add-to-list 'package-archives '("marmalade" . "http://marmalade-repo.org/packages/"))
