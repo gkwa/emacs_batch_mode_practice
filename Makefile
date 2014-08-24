82ab2ae41593fbb57299dbbb51fca944ab44b475
taylormonacelli <tailor@u.washington.edu>
2014-08-24 13:12:04 -0700

    Add more offline package install tests to debug windows installs

--- Makefile
+++ Makefile
@@ -1,6 +1,7 @@
 clean:
 	rm -f *~
 	rm -f .*~
+	$(MAKE) -C automated-testing-packages clean
 	$(MAKE) -C t1 clean
 	$(MAKE) -C minimal-package-test clean
 	$(MAKE) -C elpa_marmelade_auto_install clean
---
+++ automated-testing-packages/build.el
@@ -0,0 +1,10 @@
+(package-initialize)
+(package-refresh-contents)
+(let ((package
+       (concat
+        (file-name-directory
+         (or (buffer-file-name)
+             load-file-name))
+        (car (reverse command-line-args)))))
+  (message "the package is: %s" package)
+  (package-install-file package))
