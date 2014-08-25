e16ae3d9d165e916e46321d0054ebc3b55bec938
taylormonacelli <tailor@u.washington.edu>
2014-08-24 20:43:32 -0700

    yada-yada

--- try-use-package/emacs-install-clean.el
+++ try-use-package/emacs-install-clean.el
@@ -20,9 +20,6 @@
 	  ".elpa"))
 
    (setq package-archives ())
-   (add-to-list 'package-archives '("elpa" . "http://tromey.com/elpa/"))
-   (add-to-list 'package-archives '("marmalade" . "http://marmalade-repo.org/packages/"))
-   (add-to-list 'package-archives '("melpa" . "http://melpa.milkbox.net/packages/"))
    (add-to-list 'package-archives '("melpa-stable" . "http://melpa-stable.milkbox.net/packages/") t) ;; http://emacsredux.com/blog/2014/05/16/melpa-stable
 
    (package-initialize)
